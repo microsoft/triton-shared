e8072835d483bc84120a9fe81f8531b0ac0a3409
Nhat Nguyen <hoangnhat2911@gmail.com>
2024-06-14 19:34:08 -0400

    seems ok

--- lib/Conversion/StructuredToMemref/StructuredToMemrefPass.cpp
+++ lib/Conversion/StructuredToMemref/StructuredToMemrefPass.cpp
@@ -5,7 +5,9 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/BuiltinTypes.h"
@@ -14,6 +16,8 @@
 #include "triton-shared/Dialect/TritonStructured/IR/TritonStructuredDialect.h"
 #include "triton-shared/Dialect/TritonTilingExt/IR/TritonTilingExtDialect.h"
 #include "triton/Dialect/Triton/IR/Dialect.h"
+
+#include "mlir/Transforms/OneToNTypeConversion.h"
 
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Bufferization/IR/Bufferization.h"
@@ -75,6 +79,86 @@ public:
   }
 };
 
+struct ScalarAddptrConverter
+    : public OneToNOpConversionPattern<triton::AddPtrOp> {
+  using OneToNOpConversionPattern::OneToNOpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(triton::AddPtrOp op, OpAdaptor adaptor,
+                  OneToNPatternRewriter &rewriter) const override {
+    if (isa<ShapedType>(op.getType())) {
+      return failure();
+    }
+
+    auto loc = op->getLoc();
+
+    auto offsetIndex = rewriter.create<arith::IndexCastOp>(
+        loc, rewriter.getIndexType(), op.getOffset());
+
+    auto layout =
+        StridedLayoutAttr::get(op.getContext(), ShapedType::kDynamic, {1});
+
+    auto elemType =
+        cast<triton::PointerType>(op.getPtr().getType()).getPointeeType();
+    auto memrefType = MemRefType::get({1}, elemType, layout);
+
+    auto ptrInfo = adaptor.getPtr();
+
+    if (ptrInfo.size() == 1) {
+
+      auto castOp = rewriter.create<memref::ReinterpretCastOp>(
+          loc, memrefType, ptrInfo[0],
+          getAsOpFoldResult(offsetIndex) /*offset*/,
+          ArrayRef<OpFoldResult>{rewriter.getIndexAttr(1)} /*sizes*/,
+          ArrayRef<OpFoldResult>{rewriter.getIndexAttr(1)} /*strides*/);
+
+      rewriter.replaceOp(op,
+                         SmallVector<Value>{castOp.getResult(), offsetIndex},
+                         adaptor.getResultMapping());
+
+    } else {
+      auto ptr = ptrInfo[0];
+      auto offset = ptrInfo[1];
+
+      auto newOffset = rewriter.create<arith::AddIOp>(loc, offset, offsetIndex);
+
+      auto castOp = rewriter.create<memref::ReinterpretCastOp>(
+          loc, memrefType, ptr, getAsOpFoldResult(newOffset) /*offset*/,
+          ArrayRef<OpFoldResult>{rewriter.getIndexAttr(1)} /*sizes*/,
+          ArrayRef<OpFoldResult>{rewriter.getIndexAttr(1)} /*strides*/);
+
+      rewriter.replaceOp(op, SmallVector<Value>{castOp.getResult(), newOffset},
+                         adaptor.getResultMapping());
+    }
+
+    return success();
+  }
+};
+
+static std::optional<SmallVector<Value>>
+buildGetTupleElementOps(OpBuilder &builder, TypeRange resultTypes, Value input,
+                        Location loc) {
+  SmallVector<Value> res;
+  auto castOp = input.getDefiningOp<UnrealizedConversionCastOp>();
+  res.push_back(castOp->getOperand(0));
+  res.push_back(
+      builder.create<arith::ConstantOp>(loc, builder.getIndexAttr(0)));
+  for (auto t : resultTypes) {
+    t.dump();
+  }
+  return res;
+}
+
+static std::optional<Value> buildMakeTupleOp(OpBuilder &builder,
+                                             Type resultType, ValueRange inputs,
+                                             Location loc) {
+  resultType.dump();
+  for (auto i : inputs) {
+    i.dump();
+  }
+  return inputs[0];
+}
+
 class StructuredToMemrefPass
     : public triton::impl::StructuredToMemrefBase<StructuredToMemrefPass> {
   using StructuredToMemrefBase<StructuredToMemrefPass>::StructuredToMemrefBase;
@@ -107,46 +191,74 @@ public:
   }
 
   void runOnOperation() override {
+    auto moduleOp = getOperation();
 
     if (failed(convertArgsToMemrefType())) {
       signalPassFailure();
       return;
     }
-
-    auto moduleOp = getOperation();
-
     RewritePatternSet patterns(&getContext());
-    ConversionTarget target(getContext());
 
-    target.addLegalDialect<
-        func::FuncDialect, arith::ArithDialect, math::MathDialect,
-        linalg::LinalgDialect, affine::AffineDialect, scf::SCFDialect,
-        cf::ControlFlowDialect, tensor::TensorDialect,
-        bufferization::BufferizationDialect, ttx::TritonTilingExtDialect,
-        memref::MemRefDialect>();
+    auto context = &getContext();
+    OneToNTypeConverter converter;
+    converter.addConversion([](Type type) { return type; });
+    converter.addConversion(
+        [context](triton::PointerType ptrType, SmallVectorImpl<Type> &types)
+            -> std::optional<LogicalResult> {
+          SmallVector<int64_t> strides{1};
+          auto layout =
+              StridedLayoutAttr::get(context, ShapedType::kDynamic, strides);
 
-    target.addIllegalDialect<tts::TritonStructuredDialect>();
+          auto elemType = ptrType.getPointeeType();
+          auto memrefType = MemRefType::get({1}, elemType, layout);
 
-    target.addDynamicallyLegalOp<UnrealizedConversionCastOp>([](Operation *op) {
-      auto resType = op->getResultTypes()[0];
-      return !isa<triton::PointerType>(resType);
-    });
+          types = SmallVector<Type>{memrefType, IndexType::get(context)};
+          return success();
+        });
 
-    LoopTypeConverter loopTypeConverter(patterns.getContext());
-    mlir::scf::populateSCFStructuralTypeConversionsAndLegality(
-        loopTypeConverter, patterns, target);
-    triton::populateStructuredToMemrefConversionPatterns(patterns,
-                                                         loopTypeConverter);
+    converter.addArgumentMaterialization(buildMakeTupleOp);
+    converter.addTargetMaterialization(buildGetTupleElementOps);
 
-    if (failed(applyPartialConversion(moduleOp, target, std::move(patterns)))) {
-      signalPassFailure();
-    }
+    patterns.add<ScalarAddptrConverter>(converter, context);
 
-    // Erase dead code and fold constants created during lowering
-    PassManager pm(&getContext(), moduleOp.getOperationName());
-    pm.addPass(createCanonicalizerPass());
-    if (failed(runPipeline(pm, getOperation()))) {
-      signalPassFailure();
+    scf::populateSCFStructuralOneToNTypeConversions(converter, patterns);
+
+    // triton::populateStructuredToMemrefConversionPatterns(patterns,
+    // converter);
+
+    if (failed(applyPartialOneToNConversion(getOperation(), converter,
+                                            std::move(patterns))))
+      return signalPassFailure();
+
+    moduleOp->dump();
+    // return;
+
+    // auto moduleOp = getOperation();
+    {
+      RewritePatternSet patterns(&getContext());
+      ConversionTarget target(getContext());
+
+      target.addLegalDialect<
+          func::FuncDialect, arith::ArithDialect, math::MathDialect,
+          linalg::LinalgDialect, affine::AffineDialect, scf::SCFDialect,
+          cf::ControlFlowDialect, tensor::TensorDialect,
+          bufferization::BufferizationDialect, ttx::TritonTilingExtDialect,
+          memref::MemRefDialect>();
+
+      target.addIllegalDialect<tts::TritonStructuredDialect>();
+
+      target.addDynamicallyLegalOp<UnrealizedConversionCastOp>(
+          [](Operation *op) {
+            auto resType = op->getResultTypes()[0];
+            return !isa<triton::PointerType>(resType);
+          });
+
+      // Erase dead code and fold constants created during lowering
+      PassManager pm(&getContext(), moduleOp.getOperationName());
+      pm.addPass(createCanonicalizerPass());
+      if (failed(runPipeline(pm, getOperation()))) {
+        signalPassFailure();
+      }
     }
   }
 };
