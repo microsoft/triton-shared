b818a9ee3be3635146d703f3503bb9e7e65041df
Nhat Nguyen <hoangnhat2911@gmail.com>
2024-09-06 15:47:45 -0400

    Fix

--- backend/compiler.py
+++ backend/compiler.py
@@ -1,7 +1,8 @@
 from triton.backends.compiler import BaseBackend, GPUTarget
 from triton._C.libtriton import ir, passes
 from dataclasses import dataclass
-from typing import Any, Tuple
+from typing import Any, Dict, Tuple
+from types import ModuleType
 import hashlib
 import tempfile
 import os
@@ -192,3 +193,6 @@ class CPUBackend(BaseBackend):
     @functools.lru_cache()
     def hash(self):
         return self.target
+
+    def get_module_map(self) -> Dict[str, ModuleType]:
+        return {}
