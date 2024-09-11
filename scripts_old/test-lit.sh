384d9daca2da538f939c10dfae2bc511c940a71b
Nhat Nguyen <hoangnhat2911@gmail.com>
2024-09-11 12:57:25 -0400

    Add scripts

---
+++ scripts_old/test-lit.sh
@@ -0,0 +1,6 @@
+cd /home/nhat/github/triton_shared/triton/python
+LIT_TEST_DIR="build/cmake.linux-x86_64-cpython-3.10//third_party/triton_shared/test"
+if [ ! -d "${LIT_TEST_DIR}" ]; then
+    echo "Coult not find '${LIT_TEST_DIR}'" ; exit -1
+fi
+lit -v "${LIT_TEST_DIR}"
