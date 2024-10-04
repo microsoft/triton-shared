8fe2d3869788fe264388910f8bca02231ac374b5
Nhat Nguyen <hoangnhat2911@gmail.com>
2024-10-04 18:19:07 -0400

    Add tests

--- python/examples/test_tensor_index_iterargs.py
+++ python/examples/test_tensor_index_iterargs.py
@@ -5,37 +5,6 @@ import triton.language as tl
 
 from triton.backends.triton_shared.driver import CPUDriver
 
-
-def test_nested_mask():
-    @triton.jit
-    def nested(In, Out):
-        offsets = tl.arange(0, 2)
-        out_offsets = tl.arange(0, 2)
-        for i in range(0, 2):
-            offsets += 1 # [1, 2], [4, 5]
-            for j in range(0, 2):
-                offsets += 1 # [2, 3], [3, 4], [5, 6], [6, 7]
-                a1 = tl.load(In + offsets, mask=offsets < 9)
-                tl.store(Out + out_offsets, a1)
-                out_offsets += 2
-
-    input = torch.arange(0, 8, device='cpu', dtype=torch.int32)
-    output = torch.full((8,), 88, device='cpu', dtype=torch.int32)
-
-    triton.runtime.driver.set_active(CPUDriver())
-    # input = torch.arange(0, 8, device='cuda', dtype=torch.int32)
-    # output = torch.full((8,), 88, device='cuda', dtype=torch.int32)
-
-    grid = lambda meta: (1,)
-
-    print(output)
-    nested[grid](input, output)
-    # addptr_outofbounds[grid](input, output, l)
-    print(input)
-    print(output)
-    # assert torch.equal(input, output)
-
-
 def test_addptr_mask(device):
     @triton.jit
     def addptr_with_masks(in0, out0, mask_bound):
@@ -43,6 +12,9 @@ def test_addptr_mask(device):
         out_offs = tl.arange(0, 4)
         # We're loading 16 elements here, the bound is set to 14 so that
         # the mask only applies to the last iteration's load
+        # TODO: The current mask implementation in triton-shared does not seem
+        # to work when the mask applies to the entire tensor load, perhaps
+        # the lowerings for subviews with 0-dimensions do not work?
         for i in range(0, 4):
             mask = offs < mask_bound
             a = tl.load(in0 + offs, mask=mask, other=-11)
