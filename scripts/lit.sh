7823ca353b68855f905eb1cca33491472bd55928
Nhat Nguyen <hoangnhat2911@gmail.com>
2024-11-18 09:16:02 -0800

    fix

--- scripts/lit.sh
+++ scripts/lit.sh
@@ -1,6 +1,6 @@
 # python3 -m pip install lit
 cd /home/nhat/github/triton_shared/triton/python
-LIT_TEST_DIR="build/cmake.linux-x86_64-cpython-3.10/third_party/triton_shared/test"
+LIT_TEST_DIR="build/cmake.linux-x86_64-cpython-3.8/third_party/triton_shared/test"
 if [ ! -d "${LIT_TEST_DIR}" ]; then
     echo "Coult not find '${LIT_TEST_DIR}'" ; exit -1
 fi
