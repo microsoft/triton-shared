dc42c77056df68b55ffd1bbd975df470eceaf676
Nhat Nguyen <hoangnhat2911@gmail.com>
2025-02-12 15:20:31 -0500

    set up types

--- lib/Dialect/TPtr/IR/TPtrDialect.cpp
+++ lib/Dialect/TPtr/IR/TPtrDialect.cpp
@@ -1,10 +1,28 @@
+#include "mlir/IR/DialectImplementation.h" // required by `Types.cpp.inc`
+#include "llvm/ADT/TypeSwitch.h"           // required by `Types.cpp.inc`
+
 #include "triton-shared/Dialect/TPtr/IR/TPtrDialect.h"
 
-// using namespace mlir;
+#include "mlir/Dialect/Ptr/IR/PtrDialect.h"
+#include "mlir/Dialect/Ptr/IR/PtrTypes.h"
+
+#define GET_TYPEDEF_CLASSES
+#include "triton-shared/Dialect/TPtr/IR/TPtrTypes.cpp.inc"
+
+//===----------------------------------------------------------------------===//
+// Triton Dialect
+//===----------------------------------------------------------------------===//
+void mlir::tptr::TPtrDialect::registerTypes() {
+  addTypes<
+#define GET_TYPEDEF_LIST
+#include "triton-shared/Dialect/TPtr/IR/TPtrTypes.cpp.inc"
+      >();
+}
 
 /// Dialect creation, the instance will be owned by the context. This is the
 /// point of registration of custom types and operations for the dialect.
 void mlir::tptr::TPtrDialect::initialize() {
+  registerTypes();
   addOperations<
 #define GET_OP_LIST
 #include "triton-shared/Dialect/TPtr/IR/TPtrOps.cpp.inc"
